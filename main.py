d229cdb2730ebf7604288182db339f2bbacfb891
Khoa Nguyen <60150482+khoainho@users.noreply.github.com>
2023-03-24 23:57:28 -0500

    Error handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from uuid import UUID, uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Gender, Role, User
 
 app = FastAPI()
@@ -47,3 +47,7 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code = 404,
+        detail = f"Sorry, user with id: {user_id} does not exsits"
+    )
--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 from enum import Enum
 from typing import Optional, List
 from uuid import UUID, uuid4
